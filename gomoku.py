7766d347ee26a9f288738ccfad6c5dc18bdfa140
西田大士朗 <nishidadaishiro@team-lab.com>
2020-12-14 14:16:40 +0900

    fallback for when max fails

--- gomoku.py
+++ gomoku.py
@@ -141,17 +141,23 @@ class GreedyAgent(BaseAgent):
                         base = np.array([col, row]) + direction * offset
                         if board.get_piece(base) == self.__side:
                             combinations[dir_idx, row, col] += 32 + (1 << offset)
-                        elif board.get_piece(base) > 0:
+                        elif not board.is_on_board(base) or board.get_piece(base) > 0:
                             combinations[dir_idx, row, col] = -1
                             break
 
-        for dir, row, col in zip(*np.where(combinations//32 == np.amax(combinations//32))):
-            flags = combinations[dir, row, col] % 32
-            for offset in range(5):
-                if flags & (1 << offset) == 0:
-                    coord = np.array([col, row]) + offset * self.__DIRECTIONS[dir]
-                    if board.is_on_board(coord):
-                        return coord
+        max_count = np.amax(combinations//32)
+        for count in range(max_count, -1, -1):
+            for dir, row, col in zip(*np.where(combinations//32 == count)):
+                flags = combinations[dir, row, col] % 32
+                for offset in range(5):
+                    if flags & (1 << offset) == 0:
+                        coord = np.array([col, row]) + offset * self.__DIRECTIONS[dir]
+                        if board.is_on_board(coord):
+                            return coord
+
+        # fallback: should not reach
+        print('Fallback: return random value')
+        return np.random.randint(0, board.get_size(), 2)
 
 if __name__ == "__main__":
     # sample run
