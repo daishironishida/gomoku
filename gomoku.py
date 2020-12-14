5c0f8e87684f67a5419c986925a408ca0699e175
西田大士朗 <nishidadaishiro@team-lab.com>
2020-12-14 14:11:47 +0900

    add greedy agent

--- gomoku.py
+++ gomoku.py
@@ -121,6 +121,38 @@ class ConsoleAgent(BaseAgent):
             except Exception as e:
                 print('Invalid input! Format: [x-coord],[y-coord]')
 
+class GreedyAgent(BaseAgent):
+    __NUM_REQUIRED = 5
+    __DIRECTIONS = np.array([[1,0], [1,1], [0,1], [-1,1]])
+
+    def __init__(self, side):
+        self.__side = side
+
+    def move(self, board):
+        # value is separated into two parts:
+        # rightmost 5 bits: indicates offsets at which piece was found
+        # value divided by 64: number of pieces found
+        combinations = np.zeros((4,) + board.get_board().shape, np.int)
+
+        for row in range(board.get_size()):
+            for col in range(board.get_size()):
+                for dir_idx, direction in enumerate(self.__DIRECTIONS):
+                    for offset in range(self.__NUM_REQUIRED):
+                        base = np.array([col, row]) + direction * offset
+                        if board.get_piece(base) == self.__side:
+                            combinations[dir_idx, row, col] += 32 + (1 << offset)
+                        elif board.get_piece(base) > 0:
+                            combinations[dir_idx, row, col] = -1
+                            break
+
+        for dir, row, col in zip(*np.where(combinations//32 == np.amax(combinations//32))):
+            flags = combinations[dir, row, col] % 32
+            for offset in range(5):
+                if flags & (1 << offset) == 0:
+                    coord = np.array([col, row]) + offset * self.__DIRECTIONS[dir]
+                    if board.is_on_board(coord):
+                        return coord
+
 if __name__ == "__main__":
     # sample run
     def make_agent_move(game, agent, side):
@@ -134,7 +166,7 @@ if __name__ == "__main__":
     GAME_SIZE = 7
 
     game = GameManager(GAME_SIZE)
-    agent1 = RandomAgent(GAME_SIZE)
+    agent1 = GreedyAgent(1)
     agent2 = ConsoleAgent(2)
 
     assert isinstance(agent1, BaseAgent)
