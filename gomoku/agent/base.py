005c8f22fb381443f3085e4c0c0d2f0fc3115b87
西田大士朗 <nishidadaishiro@team-lab.com>
2020-12-19 18:57:28 +0900

    choose agent from command line arguments

--- gomoku/agent/base.py
+++ gomoku/agent/base.py
@@ -2,6 +2,9 @@ from abc import ABCMeta, abstractmethod
 import numpy as np
 
 class BaseAgent(metaclass=ABCMeta):
+    def __init__(self, side):
+        self._side = side
+
     @abstractmethod
     def move(self, board):
         pass
@@ -11,13 +14,9 @@ class RandomAgent(BaseAgent):
         return np.random.randint(0, board.get_size(), 2)
 
 class ConsoleAgent(BaseAgent):
-    def __init__(self, side):
-        self.__side = side
-        print(f'Initializing console agent for side {self.__side}')
-
     def move(self, board):
         while True:
-            print(f'Player {self.__side}, type your next move! Format: [x-coord],[y-coord]')
+            print(f'Player {self._side}, type your next move! Format: [x-coord],[y-coord]')
             user_input = input()
             try:
                 coords = str(user_input).split(',')
